143ef481ff9cacb17ca04cf81bb599218a5a4d02
Tomer <babygang21@proton.me>
2025-05-09 05:51:52 +0300

    Update npcs.lua

--- d3-dvall/npcs.lua
+++ d3-dvall/npcs.lua
@@ -1,41 +1,41 @@
 Citizen.CreateThread(function()
     local enableSpawns = false
 
-    -- Timer to enable spawning every 10 minutes for 30 seconds
+ 
     Citizen.CreateThread(function()
         while true do
             enableSpawns = true
-            Wait(30000) -- Keep spawning enabled for 30 seconds
+            Wait(30000) 
             enableSpawns = false
-            Wait(600000 - 30000) -- Wait remaining 9.5 minutes
+            Wait(600000 - 30000) 
         end
     end)
 
-    -- Main loop to control density based on enableSpawns
+ 
     while true do
         if enableSpawns then
-            -- Allow some traffic and NPCs when enabled
+    
             SetPedDensityMultiplierThisFrame(0.7)
             SetScenarioPedDensityMultiplierThisFrame(0.7, 0.7)
             SetVehicleDensityMultiplierThisFrame(0.7)
             SetRandomVehicleDensityMultiplierThisFrame(0.7)
             SetParkedVehicleDensityMultiplierThisFrame(0.5)
 
-            -- Allow scenarios
+     
             SetScenarioTypeEnabled("WORLD_VEHICLE_PARK_PERPENDICULAR_NOSE_IN", true)
             SetScenarioTypeEnabled("WORLD_VEHICLE_PARK_PARALLEL", true)
             SetScenarioTypeEnabled("WORLD_VEHICLE_DRIVE_SOLO", true)
             SetScenarioTypeEnabled("WORLD_VEHICLE_DRIVE_PASSENGERS", true)
             SetScenarioTypeEnabled("WORLD_VEHICLE_BIKE_OFF_ROAD_RACE", true)
 
-            -- Enable cops, garbage trucks, boats
+       
             SetGarbageTrucks(true)
             SetRandomBoats(true)
             SetCreateRandomCops(true)
             SetCreateRandomCopsNotOnScenarios(true)
             SetCreateRandomCopsOnScenarios(true)
         else
-            -- Disable everything
+            
             SetPedDensityMultiplierThisFrame(0.0)
             SetScenarioPedDensityMultiplierThisFrame(0.0, 0.0)
             SetVehicleDensityMultiplierThisFrame(0.0)
